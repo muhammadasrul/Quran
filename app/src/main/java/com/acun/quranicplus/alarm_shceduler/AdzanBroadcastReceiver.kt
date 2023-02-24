66f511a33d5e2496f18472871cebeff85ba02438
Muhammad Asrul <asrulajipangestu@gmail.com>
2023-02-24 20:21:17 +0700

    enhance adzan notification

--- app/src/main/java/com/acun/quranicplus/alarm_shceduler/AdzanBroadcastReceiver.kt
+++ app/src/main/java/com/acun/quranicplus/alarm_shceduler/AdzanBroadcastReceiver.kt
@@ -1,63 +1,56 @@
 package com.acun.quranicplus.alarm_shceduler
 
-import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.os.Build
-import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.acun.quranicplus.R
 import com.acun.quranicplus.util.EXTRA_ALARM
 
 class AdzanBroadcastReceiver: BroadcastReceiver() {
 
+    companion object {
+        const val CHANNEL_ID = "Prayer Reminder"
+    }
+
     override fun onReceive(context: Context, intent: Intent?) {
         val name = intent?.getStringExtra(EXTRA_ALARM)
 
-        Log.d("waduh", "onReceive: $name")
-        Toast.makeText(context, name, Toast.LENGTH_SHORT).show()
-
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
 
-        val notification = NotificationCompat.Builder(context, "adzan")
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.icon)
-            .setContentTitle("Test Notif Adzan")
-            .setContentText("$name misale")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentTitle("Prayer Reminder")
+            .setContentText("Now it's time for $name pray")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setAutoCancel(true)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                "adzan",
-                "Adzan",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
+                CHANNEL_ID,
+                CHANNEL_ID,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                enableVibration(true)
+                vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+            }
 
             notificationManager?.createNotificationChannel(channel)
         }
 
-        with(NotificationManagerCompat.from(context)) {
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                // return
+        notificationManager?.notify(name.hashCode(), notification.build())
+
+        val audioResource = if (name == "Fajr") R.raw.adzan_fajr else R.raw.adzan
+        MediaPlayer
+            .create(context, audioResource)
+            .apply {
+                isLooping = false
+                start()
             }
-            notify(101, notification.build())
-        }
     }
 }
